05afe401787e9520be746e534cc3f7a2a49a368d
SlouMo <vovankuz007@mail.ru>
2020-03-10 21:40:52 +0300

    the problem is solved: Binary representation of a number / Print a binary representation of a given number and the number of units encountered there. /

---
+++ src/NumberSystemTranslation.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NumberSystemTranslation {
+    public static void main(String[] args) {
+        int countUnit = 0;
+        System.out.println("What number will be converted to binary?");
+        System.out.println("Entered number: ");
+        Scanner readAnsw = new Scanner(System.in);
+        int userNumber = readAnsw.nextInt();
+        long valueBinary = Long.parseLong(Long.toBinaryString(userNumber));
+        while (valueBinary > 0) {
+            if (valueBinary % 10 == 1) {
+                countUnit++;
+            }
+            valueBinary /= 10;
+        }
+        System.out.println("Your binary number looks like this: " + Long.parseLong(Long.toBinaryString(userNumber)));
+        System.out.println("The number of units in your number: " + countUnit);
+    }
+}
