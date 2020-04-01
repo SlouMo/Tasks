4418d0df6e7ee567a614ec440abb063035b24561
SlouMo <vovankuz007@mail.ru>
2020-04-02 03:53:33 +0400

    the problem is solved: Search for a subarray.  / The user defines two arrays. Print the index of the beginning of the occurrence of the first array in the second. You cannot use any ready-made functions for searching and comparing arrays.  /

--- src/SearchForSubarray.java
+++ src/SearchForSubarray.java
@@ -1,17 +1,55 @@
-/*
 import java.util.Scanner;
 
 public class SearchForSubarray {
     public static void main(String[] args) {
         System.out.println("Enter the first array");
         System.out.print("Here please: ");
-        Scanner input = new Scanner(System.in);
-        int array = input.nextInt();
-        int [] arrayOne = new int[];
+        Scanner scannerInput = new Scanner(System.in);
+        String inputOne = scannerInput.nextLine();
+        int[] arrayOne = convertStringToInt(inputOne);
+        System.out.println("Enter the second array");
+        System.out.print("Here please: ");
+        String inputTwo = scannerInput.nextLine();
+        int[] arrayTwo = convertStringToInt(inputTwo);
+        boolean arrayEntry = false;
+        int startOfEntry = 0;
+        for (int i = 0; i < arrayTwo.length; i++) {
+            if (arrayTwo[i] == arrayOne[0]) {
+                startOfEntry = i;
+                arrayEntry = entryCheck(arrayOne, arrayTwo, i);
+                if (arrayEntry == true) {
+                    break;
+                }
+            }
+        }
+        if (arrayEntry == false) {
+            System.out.println("No occurrences found.");
+        } else {
+            System.out.println("Entries start with item number: " + ++startOfEntry);
+        }
+    }
 
+    public static boolean entryCheck(int[] arrayOne, int[] arrayTwo, int startOfEntry) {
+        boolean entry = true;
+        for (int i = 1; i < arrayOne.length; i++) {
+            try {
+                if (arrayOne[i] != arrayTwo[startOfEntry + i]) {
+                    entry = false;
+                }
+            } catch (Exception ex) {
+                entry = false;
+                break;
+            }
+        }
+        return entry;
+    }
 
+    public static int[] convertStringToInt(String str) {
+        String[] strArr = str.split(" ");
+        int[] numArr = new int[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            numArr[i] = Integer.parseInt(strArr[i]);
+        }
+        return numArr;
     }
 }
-
-
- */
