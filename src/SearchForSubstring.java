c6ff4c66ca0d6ddb0aa7b55990bfc6b51af4ac29
SlouMo <vovankuz007@mail.ru>
2020-04-02 06:08:03 +0400

    the problem is solved: Substring search.  / Find the index (number of the first character) of occurrence of one line in another. You cannot use string comparison and search libraries.  /

---
+++ src/SearchForSubstring.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class SearchForSubstring {
+    public static void main(String[] args) {
+        System.out.println("Enter the first string");
+        System.out.print("Here please: ");
+        Scanner scannerInput = new Scanner(System.in);
+        String inputOne = scannerInput.nextLine();
+        int[] arrayOne = convertStringToInt(inputOne);
+        System.out.println("Enter the second string");
+        System.out.print("Here please: ");
+        String inputTwo = scannerInput.nextLine();
+        int[] arrayTwo = convertStringToInt(inputTwo);
+        boolean arrayEntry = false;
+        int startOfEntry = 0;
+        if (arrayOne.length < arrayTwo.length) {
+            for (int i = 0; i < arrayTwo.length; i++) {
+                if (arrayTwo[i] == arrayOne[0]) {
+                    startOfEntry = i;
+                    arrayEntry = entryCheck(arrayOne, arrayTwo, i);
+                    if (arrayEntry == true) {
+                        break;
+                    }
+                }
+            }
+        } else {
+            for (int i = 0; i < arrayOne.length; i++) {
+                if (arrayOne[i] == arrayTwo[0]) {
+                    startOfEntry = i;
+                    arrayEntry = entryCheck(arrayTwo, arrayOne, i);
+                    if (arrayEntry == true) {
+                        break;
+                    }
+                }
+            }
+        }
+        if (arrayEntry == false) {
+            System.out.println("No occurrences found.");
+        } else {
+            System.out.println("Entries start with item number: " + ++startOfEntry);
+        }
+    }
+
+    public static boolean entryCheck(int[] arrayOne, int[] arrayTwo, int startOfEntry) {
+        boolean entry = true;
+        for (int i = 1; i < arrayOne.length; i++) {
+            try {
+                if (arrayOne[i] != arrayTwo[startOfEntry + i]) {
+                    entry = false;
+                }
+            } catch (Exception ex) {
+                entry = false;
+                break;
+            }
+        }
+        return entry;
+    }
+
+    public static int[] convertStringToInt(String str) {
+        String[] strArr = str.split(" ");
+        int[] numArr = new int[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            numArr[i] = Integer.parseInt(strArr[i]);
+        }
+        return numArr;
+    }
+}
